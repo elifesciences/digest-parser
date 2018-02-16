fe9825f57b5067bedc5c598130ce949ec3cf886e
Graham Nott <gnott@starglobal.ca>
2018-02-15 17:32:50 -0800

    Rough docx parsing with a test case comparing all the parsed content.

--- digestparser/parse.py
+++ digestparser/parse.py
@@ -1,3 +1,59 @@
+from docx import Document
 
-def dummy():
-    return None
+def parse_content(file_name):
+    "return all the content for testing"
+    content = ''
+    document = Document(file_name)
+    #print(document.core_properties.author)
+    #print(document.paragraphs)
+    for para in document.paragraphs:
+        content += join_runs(para.runs) + "\n"
+    return content
+
+
+def open_close_style(run, prev_run, output, attr='italic'):
+    open_tag = None
+    close_tag = None
+    if attr == 'italic':
+        open_tag = '<i>'
+        close_tag = '</i>'
+    if attr == 'bold':
+        open_tag = '<b>'
+        close_tag = '</b>'
+    if not open_tag or not close_tag:
+        return output
+    # continue
+    run_contains_break = bool(run.text.endswith("\n"))
+    prev_run_contains_break = bool(prev_run.text.endswith("\n") if prev_run is not None else False)
+    # add the close tag first
+    if (
+        (prev_run_contains_break and getattr(prev_run, attr) is True) or
+        (getattr(run, attr) is not True and prev_run and getattr(prev_run, attr) is True)
+        ):
+        # check for new line
+        if output.endswith("\n"):
+            output = output.rstrip("\n") + close_tag + "\n"
+        else:
+            output += close_tag
+    # add the open tag
+    if (
+        (prev_run_contains_break and getattr(run, attr) is True) or
+        (getattr(run, attr) is True and (prev_run is None or getattr(prev_run, attr) is not True))
+        ):
+        output += open_tag
+    return output
+
+def join_runs(runs):
+    output = ''
+    prev_run = None
+    for run in runs:
+        output = open_close_style(run, prev_run, output, 'italic')
+        output = open_close_style(run, prev_run, output, 'bold')
+        output += run.text
+        prev_run = run
+    return output
+
+if __name__ == "__main__":
+    "debug while developing"
+    content = parse_content('tests/test_data/DIGEST 99999.docx')
+    print(content.encode('utf-8'))
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,14 +1,33 @@
 import unittest
 import time
+import os
 from digestparser import parse
+
+
+def read_file(file_name_plus_path):
+    with open(file_name_plus_path, 'rb') as open_file:
+        return open_file.read()
+
+def fixture_path(file_name):
+    return os.path.join('tests', 'fixtures', file_name)
+
+def read_fixture(file_name):
+    return read_file(fixture_path(file_name))
+
+def test_data_path(file_name):
+    return os.path.join('tests', 'test_data', file_name)
+
 
 class TestGenerate(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    def test_dummy(self):
-        self.assertEqual(parse.dummy(), None)
+    def test_parse_content(self):
+        "test parsing all the content"
+        expected = read_fixture('digest_99999.txt').decode('utf-8')
+        content = parse.parse_content(test_data_path('DIGEST 99999.docx'))
+        self.assertEqual(content, expected)
 
 
 if __name__ == '__main__':
