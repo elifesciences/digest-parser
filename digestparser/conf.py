a1f009d9af2f6adc39cdca54395c9d74c0000efc
Graham Nott <gnott@starglobal.ca>
2018-03-21 18:30:56 -0700

    Refactoring the conf module for better test coverage, and tests of different conf value types.

--- digestparser/conf.py
+++ digestparser/conf.py
@@ -1,31 +1,48 @@
 import configparser as configparser
 import json
 
-config = configparser.ConfigParser(interpolation=None)
-config.read('digest.cfg')
+CONFIG_FILE = 'digest.cfg'
+BOOLEAN_VALUES = []
+INT_VALUES = []
+LIST_VALUES = []
 
-def raw_config(config_section):
+def load_config(config_file=CONFIG_FILE):
+    config = configparser.ConfigParser(interpolation=None)
+    config.read(config_file)
+    return config
+
+def raw_config(config_section, config_file=CONFIG_FILE):
     "try to load the config section"
+    config = load_config(config_file)
     if config.has_section(config_section):
         return config[config_section]
     # default
     return config.defaults()
 
+def boolean_config(raw_config, value_name):
+    "extract the value as a boolean"
+    return raw_config.getboolean(value_name)
+
+def int_config(raw_config, value_name):
+    "extract the value as an int"
+    return raw_config.getint(value_name)
+
+def list_config(raw_config, value_name):
+    "extract the value as a list parsed from JSON"
+    return json.loads(raw_config.get(value_name))
+
 def parse_raw_config(raw_config):
     "parse the raw config to something good"
-    digest_config = {}
-    boolean_values = []
-    int_values = []
-    list_values = []
+    config_dict = {}
 
     for value_name in raw_config:
-        if value_name in boolean_values:
-            digest_config[value_name] = raw_config.getboolean(value_name)
-        elif value_name in int_values:
-            digest_config[value_name] = raw_config.getint(value_name)
-        elif value_name in list_values:
-            digest_config[value_name] = json.loads(raw_config.get(value_name))
+        if value_name in BOOLEAN_VALUES:
+            config_dict[value_name] = boolean_config(raw_config, value_name)
+        elif value_name in INT_VALUES:
+            config_dict[value_name] = int_config(raw_config, value_name)
+        elif value_name in LIST_VALUES:
+            config_dict[value_name] = list_config(raw_config, value_name)
         else:
             # default
-            digest_config[value_name] = raw_config.get(value_name)
-    return digest_config
+            config_dict[value_name] = raw_config.get(value_name)
+    return config_dict
